acc0f8cc0d3e495d6986f46cee4e15070e78e7a3
Michelle-Lohwt <michellelohwt0799@gmail.com>
2024-08-22 11:05:46 +0800

    refactor(metrics): Change to classification evaluation metrics

--- pipelines/deployment_pipeline.py
+++ pipelines/deployment_pipeline.py
@@ -119,8 +119,9 @@ def continuous_deployment_pipeline(
   df = ingest_data(data_path=data_path)
   X_train, X_test, y_train, y_test = clean_data(df)
   model = train_model(X_train, X_test, y_train, y_test)
-  r2_score, rmse = evaluate_model(model, X_test, y_test)
-  deployment_decision = deployment_trigger(r2_score)
+  # r2_score, rmse = evaluate_model(model, X_test, y_test)
+  accuracy, precision = evaluate_model(model, X_test, y_test)
+  deployment_decision = deployment_trigger(accuracy)
   mlflow_model_deployer_step(
     model = model,
     deploy_decision = deployment_decision,
